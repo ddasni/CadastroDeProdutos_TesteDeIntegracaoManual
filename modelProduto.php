717929bed14b6c047aed1b301c53f6f0e4c155e5
ddasni <danielsousamelo5@gmail.com>
2025-05-15 11:16:26 -0300

    Adicionando a verificação

--- modelProduto.php
+++ modelProduto.php
@@ -4,11 +4,19 @@ include "conexao.php";
 class modelProduto {
     public function inserir(Produto $produto) {
 
+        // verificação dos dados, se possui dados obrigatorios e se o valor não é negativo
+        if (!$produto->getNome() || !$produto->getDescricao() || $produto->getPreco() <= 0) {
+
+            echo "Dados inválidos.";
+
+            return;
+        }
+
         $sql = "INSERT INTO produtos(nome, preco, descricao) VALUES (?, ?, ?)";
         $con = new Conexao();
         $bd = $con->pegarConexao();
         
-        
+
         $stm = $bd->prepare($sql);
         $stm->bindValue(1, $produto->getNome());
         $stm->bindValue(2, $produto->getPreco());
